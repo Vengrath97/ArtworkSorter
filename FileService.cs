153d1289d262daa32ad266cb421a667c48192942
Mikołaj Wojtas <vengrath97@gmail.com>
2021-12-11 12:35:45 +0100

    Removed old, usused methods

--- FileService.cs
+++ FileService.cs
@@ -23,21 +23,6 @@ namespace VenConSort
             }
             Console.ReadKey();
         }
-        public static void OLDSaveMemoryAsFile(string FilePath, List<Artwork> List)
-        {
-            File.WriteAllText(FilePath, "");
-            foreach (Artwork item in List)
-            {
-                File.AppendAllText(FilePath, item.GetTitle());
-                File.AppendAllText(FilePath, "\n");
-                File.AppendAllText(FilePath, item.GetYear().ToString());
-                File.AppendAllText(FilePath, "\n");
-                File.AppendAllText(FilePath, item.GetGenre());
-                File.AppendAllText(FilePath, "\n");
-                File.AppendAllText(FilePath, item.GetArtist());
-                File.AppendAllText(FilePath, "\n");
-            }
-        }
         public static void SaveMemoryAsFile(string FilePath, List<Artwork> List)
         {
             File.WriteAllText(FilePath, "");
@@ -57,19 +42,6 @@ namespace VenConSort
             }
             return artworklist;
         }
-        public static List<Artwork> OLDLoadMoviesFromFile(string FilePath, List<Artwork> artworklist)
-        {
-            string[] ArtworkListFull = System.IO.File.ReadAllLines(FilePath);
-            for (int i = 0; 4 * i < ArtworkListFull.Length; i += 1)
-            {
-                artworklist.Add(new Movie());
-                artworklist[i].SetTitle(ArtworkListFull[4 * i]);
-                artworklist[i].SetYear(int.Parse(ArtworkListFull[4 * i + 1]));
-                artworklist[i].SetGenre(ArtworkListFull[4 * i + 2]);
-                artworklist[i].SetArtist(ArtworkListFull[4 * i + 3]);
-            }
-            return artworklist;
-        }
         public static List<Artwork> LoadSongsFromFile(string FilePath, List<Artwork> artworklist)
         {
             string[] ArtworkListFull = System.IO.File.ReadAllLines(FilePath);
@@ -80,20 +52,6 @@ namespace VenConSort
             }
             return artworklist;
         }
-        public static List<Artwork> OLDLoadSongsFromFile(string FilePath, List<Artwork> artworklist)
-        {
-            string[] ArtworkListFull = System.IO.File.ReadAllLines(FilePath);
-            for (int i = 0; 4 * i < ArtworkListFull.Length; i += 1)
-            {
-                artworklist.Add(new Song());
-                artworklist[i].SetTitle(ArtworkListFull[4 * i]);
-                artworklist[i].SetYear(int.Parse(ArtworkListFull[4 * i + 1]));
-                artworklist[i].SetGenre(ArtworkListFull[4 * i + 2]);
-                artworklist[i].SetArtist(ArtworkListFull[4 * i + 3]);
-            }
-            return artworklist;
-        }
-
         public static void Initialize(string MovielistPath, List<Artwork> MovieList, string SonglistPath, List<Artwork> SongList)
         {
             FileCheck(MovielistPath);
